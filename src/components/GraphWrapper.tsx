f05ec64bf761e8f2664dd74ececb721c202a854d
orkunDEV <orkunatasoy1996@live.de>
2025-12-28 17:14:28 +0100

    feat: slim comparison toggle layout

--- src/components/GraphWrapper.tsx
+++ src/components/GraphWrapper.tsx
@@ -90,7 +90,7 @@ const GraphWrapper = ({
         )}
 
         {customComparisonToggle && (
-          <div className="flex items-center gap-2 ml-auto">{customComparisonToggle}</div>
+          <div className="flex gap-2 ml-auto">{customComparisonToggle}</div>
         )}
       </div>
     </div>
